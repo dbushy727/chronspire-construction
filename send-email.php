ca43c4fb34390c263c41c11ee14776c30a4ccc45
Daniel Bushkanets <daniel.bushkanets@ppfa.org>
2017-03-08 00:25:39 -0500

    break out if no email

--- send-email.php
+++ send-email.php
@@ -1,6 +1,10 @@
 <?php
 
-$email = $_REQUEST['email'];
+if (isset($_REQUEST['email'])) {
+    $email = $_REQUEST['email'];
+} else {
+    return;
+}
 
 mail('d.bushkanets@gmail.com', 'Email Sign Up', "Email: {$email}");
 
